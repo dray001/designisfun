24bdbb9cf59cc7ef6a03c0a1f6f96fbd2b2c36dc
Mac <user@Macs-MacBook-Air.local>
2025-07-12 09:05:28 +0100

    configurations

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/designisfun', // ✅ Add this (see below!)
-  assetPrefix: '/designisfun',
   images: {
     unoptimized: true,
   },
-  experimental: {
-    viewTransition: true,
-  }
+  assetPrefix: './',
 };
 
 module.exports = nextConfig;
