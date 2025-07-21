3445e4ad66ee3d65990c5b31b0d951fe88de514d
Mac <user@Macs-MacBook-Air.local>
2025-07-21 18:20:22 +0100

    fixed routing issue 1.1

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: './',
-};
-
-module.exports = nextConfig;
+  output: 'export', // only if you're doing static export
+}
+module.exports = nextConfig
